14989b0b24feda1b33dc0563922c35ce5082fa36
unknown <ricardofariasdesouza@hotmail.com>
2021-01-16 13:29:14 -0300

    Add ProductsList

---
+++ front-web/src/Orders/OrderLocation.tsx
@@ -0,0 +1,16 @@
+function OrderLocation() {
+    return (
+        <div className="order-location-container">
+            <div className="order-location-content">
+                <h3>
+                Selecione aonde o pedido deve ser entregue:
+                </h3>
+            </div>
+            <div className="filte-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default OrderLocation;  
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,27 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
+            <OrderLocation />
         </div>
     )
 }
